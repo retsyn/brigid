881af8876d256a4da7f58788d450f6af89baeed2
Matthew Riche <matthew.riche@gmail.com>
2019-07-24 15:58:43 -0400

    Progress on sub_connect.

--- subd_connect.py
+++ subd_connect.py
@@ -8,31 +8,36 @@ import bpy
 def sort_selection():
     # Filter selected objects into list of meshes:
     meshList = []
-    boneList = []
+
+    ctlBone = None
     ctlFound = False
     for object in bpy.context.selected_objects:
+        print ("Searching through selected objects...")
+        if(object.type != 'NoneType'):
+            print ("TYPE: {}".format(object.type))
         if(object.type == 'MESH'):
             meshList.append(object)
-        if(object.type == 'BONE'):
+        if(object.type == 'ARMATURE'):
             ctlBone = object
             ctlFound = True
 
     print("Constructed mesh list from selection:\n{}".format(meshList))
     if(ctlFound):
-        print("A ctl bone(s) were in the selection.  The last one selected was {}".format())
+        print("A ctl bone(s) were in the selection.  The last one selected was {}".format(ctlBone.name))
     else:
         print("No control was found.")
     
     # Return the sorted content
-    return [meshList, boneList]
+    return { 'target_meshes':meshList, 'target_bones':ctlBone }
 
 
 def add_subd_modifier(mesh_object):
-    # Take a passed in mesh object and apply a driver.
+    # Take a passed in mesh object and apply a driver.  If it already has a driver, return that too.
     bpy.context.scene.objects.active = mesh_object
 
     subd_found = False
     for modifier in mesh_object.modifiers:
+        print ("searching through modifiers...")
         if modifier.type == "SUBSURF":
             subd_found = True
             subd_mod = modifier
@@ -47,13 +52,23 @@ def add_subd_modifier(mesh_object):
     return subd_mod
 
 
+def subd_switch_maker():
 
+    # Build dict of target content:
+    targets = sort_selection()
+    print ("using target meshes:{} and target controls: {}".format(targets['target_meshes'], targets['target_bones'] ))
 
+    if(targets['target_bones'] == ''):
+        print ("No control bone was selected; modifiers will be made but drivers will not.")
 
+    for object in targets['target_meshes']:
+        print ("Adding subd to {}...".format(object.name))
+        mod = add_subd_modifier(object)
+        # We need to edit this mod's state.
+        print ("Editing {}...".format(mod))
 
+        # We need to also add the controllers.
+        
+    print ("Done!")
 
-
-    
-
-
-
+subd_switch_maker()
