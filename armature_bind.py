fe4f50d0af7c19d5a15df02b8c14718c9930c8c4
Matthew Riche <matthew.riche@gmail.com>
2019-11-25 17:31:44 -0500

    Armature binding finished with no parenting scheme yet.

--- armature_bind.py
+++ armature_bind.py
@@ -1,12 +1,10 @@
 # Armature Binder-- Binds a lone armature into the greater structure of a rig.
-# mr_armature_bind.py
+# armature_bind.py
 # updated: 2019-11-25
-
-#  selected piece of geo and make a subd modifier. 
 
 bl_info = {
     "name": "Armature Bind",
-    "author": "Matt Riche",
+    "author": "Matt Riche, Mike Carnovale",
     "version": (1, 0),
     "blender": (2, 80, 0),
     "location": "View3D > Rigging > Armature Bind",
@@ -27,9 +25,55 @@ class BindArmatureOperator(bpy.types.Operator):
     bl_label = "Bind Add-on into Repose"
     bl_options = {'REGISTER', 'UNDO'}
 
-    def bind_armature():
+    def bind_armature(self):
         print ("Executing armature bind.")
-        pass
+        repose_armature = bpy.context.scene.TargetArmature
+        addon_armature = bpy.context.scene.SourceArmature
+        # Call the generic "join_rigs"
+        print ("DEBUG: repose is {}, and addon is {}".format(repose_armature.name, addon_armature.name))
+        self.join_rigs(repose_name = repose_armature.name, addon_name = addon_armature.name)
+        # Call the specific parent function.
+
+        return True
+
+
+    # TODO: This borrowed code from Mike used .name string to re-find the object int he 
+    # scene.  Perhaps it's more apropos to send in an object/pointer to object?
+    def join_rigs(self, repose_name, addon_name):
+        """
+        Takes bones from addon rig and makes them components of the target rig.
+        """
+        # This def's code based on Mike C.'s.
+        self.report({'INFO'}, 'Binding rigs together.')
+
+        bpy.ops.object.mode_set(mode = 'OBJECT')
+        bpy.ops.object.select_all(action = 'DESELECT')
+
+        bpy.context.scene.objects[addon_name].select_set(True)
+        bpy.context.scene.objects[repose_name].select_set(True)
+        bpy.context.view_layer.objects.active=bpy.data.objects[repose_name]
+        bpy.ops.object.join()
+        self.report({'INFO'}, 'Success!')
+
+
+    # TODO: We should consider the parenting-scheme to be some data grew this func can
+    # chew on, and be a little be more generic; like a dict that indicates what gets
+    # Parent to what, that we can parse with a generic function.  To lazy for right now.
+    def parent_teeth(self):
+        """
+        The parenting scheme specifically for teeth.
+        """
+        
+        bpy.ops.object.mode_set(mode = 'EDIT')
+
+        parent_bone=bpy.data.armatures[rig_name].edit_bones['ctl.head_fk_sub.C.001']
+        bpy.data.armatures[rig_name].edit_bones['mst.face_head.C.001'].parent=parent_bone
+
+
+    def parent_face(self):
+        self.report({'WARNING'}, 'Not ready for this yet!')
+        
+        return False
 
     def execute(self, context):
         success = self.bind_armature()
@@ -69,7 +113,6 @@ class TOOLS_PT_armature_bind(bpy.types.Panel):
 
         row = layout.row()
         row.prop(scene, "ArmatureType")
-
 
         # The button:
         row = layout.row()
