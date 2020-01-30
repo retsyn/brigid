87ff7fcfe0ba28fada3952a3f5ee111a0ebf249e
Matthew Riche <matthew.riche@gmail.com>
2020-01-30 17:56:50 -0500

    Refactor of armature_bind in hopes to add crowd bind.

--- armature_bind.py
+++ armature_bind.py
@@ -27,22 +27,44 @@ class BindArmatureOperator(bpy.types.Operator):
 
     def bind_armature(self):
         print ("Executing armature bind.")
-        repose_armature = bpy.context.scene.TargetArmature
-        addon_armature = bpy.context.scene.SourceArmature
+
+        scene = bpy.context.scene
+        repose_armature = scene.TargetArmature
+        addon_armature = scene.SourceArmature
+
+        if(repose_armature == None or addon_armature == None):
+            self.report({'WARNING'}, "Two armatures need to be selected to perform.")
+            return False
 
         repose_armature.hide_viewport=False
         addon_armature.hide_viewport=False
 
         bpy.ops.object.mode_set(mode = 'POSE')
-        # Call the generic "join_rigs"
-        success = self.join_rigs(repose_name = repose_armature.name, addon_name = addon_armature.name)
-        if(success == False):
-            self.report({'WARNING'}, "An object was missing, cancelling the process.")
-            return False
 
         # Call the specific parent function.
-        self.report({'WARNING'}, "The thing is called {}".format(repose_armature.name))
-        self.parent_teeth(repose_armature.name)
+        if(scene.ArmatureType == 'teeth'):
+
+            # This process combines armatures before in-armature parenting begins.
+            success = self.join_rigs(repose_name = repose_armature.name, addon_name = addon_armature.name)
+            if(success == False):
+                self.report({'WARNING'}, "An object was missing, cancelling the process.")
+                return False
+
+            # The 
+            self.report({'WARNING'}, "The thing is called {}".format(repose_armature.name))
+            self.parent_teeth(repose_armature.name)
+
+        if(scene.ArmatureType == 'crowd_biped'):
+            self.report({'INFO'}, "Starting crowd_biped")
+            self.parent_crowd(repose_armature.name)
+
+            success = self.join_rigs(repose_name = repose_armature.name, addon_name = addon_armature.name)
+            if(success == False):
+                self.report({'WARNING'}, "An object was missing, cancelling the process.")
+                return False
+
+
+        
 
         self.report({'INFO'}, "The binding process completed successfully.")
         return True
@@ -56,10 +78,8 @@ class BindArmatureOperator(bpy.types.Operator):
         """
         # This def's code based on Mike C.'s.
         self.report({'INFO'}, 'Binding rigs together.')
-
         bpy.ops.object.mode_set(mode='OBJECT')
         bpy.ops.object.select_all(action = 'DESELECT')
-
         self.report({'INFO'}, "Binding {} with {}".format(addon_name, repose_name))
 
         try:
@@ -79,6 +99,30 @@ class BindArmatureOperator(bpy.types.Operator):
 
         return True
 
+    
+    def match_pose(self):
+        # Snap pose position of one armature that has any smililar naming scheme.
+
+        # source and target are captured from the panel.
+        source = bpy.context.scene.SourceArmature
+        target = bpy.context.scene.TargetArmature
+
+        pose_bone_list = [bone for bone in target.pose.bones if bone.name.partition('.')[0] == 'pos']   
+
+        # Mike C's code follows.  It creates temporary copy transforms.
+        for p_bone in pose_bone_list:
+            # creates the modifier and sets the armature and bone.
+            mod_cns = p_bone.constraints.new('COPY_TRANSFORMS')
+            mod_cns.target = source
+            mod_cns.subtarget = p_bone.name
+            # print ("Snapping {}:{} to {}:{}".format(source.name, p_bone.name, target.name, p_bone.name))
+            # applies the pose.
+            bpy.ops.pose.visual_transform_apply()
+            # Context pedantic:
+            p_bone.constraints.remove(mod_cns)
+    
+        print ("Finished repose matching.")
+
 
     # TODO: We should consider the parenting-scheme to be some data grew this func can
     # chew on, and be a little be more generic; like a dict that indicates what gets
@@ -88,22 +132,36 @@ class BindArmatureOperator(bpy.types.Operator):
         The parenting scheme specifically for teeth.
         """
         bpy.ops.object.mode_set(mode = 'EDIT')
-
-        parent_bone=bpy.data.armatures[repose_name].edit_bones['def.jaw_reverse.C.001']
+        parent_bone=bpy.data.armatures[repose_name].edit_bones['ctl.teeth_upper.C.001']
         bpy.data.armatures[repose_name].edit_bones['mst.teeth_top.C.001'].parent=parent_bone
-
-        parent_bone=bpy.data.armatures[repose_name].edit_bones['def.jaw.C.001']
+        parent_bone=bpy.data.armatures[repose_name].edit_bones['ctl.teeth_lower.C.001']
         bpy.data.armatures[repose_name].edit_bones['mst.teeth_bottom.C.001'].parent=parent_bone
-
         self.report({'INFO'}, 'Teeth parented under repose head.')
+
+        # We've made some existing bones redundant:
+        try:
+            bpy.data.armatures[repose_name].edit_bones.remove('def.teeth_upper.C.001')
+            bpy.data.armatures[repose_name].edit_bones.remove('def.teeth_lower.C.001')
+        except:
+            self.report({'WARNING'}, "Old teeth def bones are already removed.")
 
         bpy.ops.object.mode_set(mode = 'OBJECT')
         return True
 
 
+    def parent_crowd(self, repose_name):
+        """
+        Specific parent scheme just for Crowd Rigs
+        """
+
+        bpy.ops.object.mode_set(mode = 'EDIT')
+
+
+
     def parent_face(self):
         self.report({'WARNING'}, 'Not ready for this yet!')        
         return False
+
 
     def execute(self, context):
         success = self.bind_armature()
@@ -122,7 +180,7 @@ class TOOLS_PT_armature_bind(bpy.types.Panel):
     bl_idname = "TOOLS_PT_armature_bind"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'Rigging'
+    bl_category = 'Repose Rig'
 
     def draw(self, context):
 
@@ -157,7 +215,7 @@ class BindArmaPropGroup(bpy.types.PropertyGroup):
 
     mode_options = [
         ('teeth', 'Teeth', '', 'Basic Teeth', 1),
-        ('face', 'Face (COMING SOON)', '', 'Face Rig', 2)
+        ('crowd_biped', 'Crowd Biped', '', 'Crowd Biped', 2)
     ]
 
     bpy.types.Scene.ArmatureType = bpy.props.EnumProperty(
